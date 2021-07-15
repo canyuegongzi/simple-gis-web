c14808b95584d5b1e1fe7388bb5df31cfdaef3a4
Marvin <yongfeide123@gmail.com>
2021-07-15 22:56:59 +0800

    feat:leaflet弹框

---
+++ src/components/dialog/line/CesiumLineDialog.vue
@@ -0,0 +1,87 @@
+<template lang="pug">
+    div()
+        CommonIconDialog(@change="changeStatus" v-show="!showFormContent")
+        div(class="drag marker-container" style="background: #ffffff" v-draw v-show="showFormContent")
+            i.el-icon-close.close(@click="changeStatus")
+            .buttons-container
+                p.title 鼠标绘制
+                el-radio-group(v-model="allowMouseRender")
+                    el-radio(:label="true") 是
+                    el-radio(:label="false") 否
+                // p.title() 点位样式
+            .buttons
+                el-button(type="primary" size="mini" @click="tapEvent('renderLine')") 渲染
+                el-button(type="primary" size="mini" @click="tapEvent('mouseRenderLine')" v-if="allowMouseRender") 鼠标绘制
+                el-button(type="primary" size="mini" @click="tapEvent('deleteLine')") 删除
+
+
+</template>
+
+<script lang="ts">
+import { Vue, Component, Emit } from 'vue-property-decorator';
+import CommonIconDialog from '../../../components/controlWidget/CommonIconDialog.vue';
+
+@Component({
+    name: 'CesiumLineDialog',
+    components: {
+        CommonIconDialog
+    }
+})
+export default class CesiumLineDialog extends Vue {
+    public visible = true;
+    public allowMouseRender = false;  // 允许鼠标绘制
+    public markerType = 1;
+    public styleType = 1;
+    public showFormContent: boolean = true; // 是否显示
+
+    /**
+     * 事件
+     * @param type
+     */
+    @Emit('map:event')
+    public tapEvent(type: string) {
+        return {
+            action: type
+        };
+    }
+
+    /**
+     * 显示状态修改
+     * @param status
+     */
+    public changeStatus(status: string) {
+        this.showFormContent = !this.showFormContent;
+    }
+}
+</script>
+
+<style scoped lang="stylus">
+.close
+    position absolute
+    top 16px
+    right 16px
+    cursor pointer
+    font-size 18px
+.marker-container
+    background #ffffff
+    border: 1px solid #909399;
+    top: 16px
+    left: 200px
+    z-index 9000 !important
+    width: 300px;
+    height: 300px;
+    border-radius: 16px;
+    padding: 0 16px 16px 16px;
+
+    .title
+        font-size: 14px;
+        font-weight: 700;
+        color: #3c4a54;
+        padding-bottom: 16px;
+        padding-top: 16px;
+
+    .buttons
+        margin-top 16px
+        display flex
+        justify-content: flex-start;
+</style>
--- src/views/baseManage/LinePage.vue
+++ src/views/baseManage/LinePage.vue
@@ -1,13 +1,60 @@
 <template lang="pug">
     .pug-container
-        p 线绘制
+        CesiumLineDialog(@map:event="cesiumMapEvent" v-if="mapType === 'CESIUM'")
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-
-@Component({})
+import CesiumLineDialog from '../../components/dialog/line/CesiumLineDialog.vue';
+import { MapTypeEnum } from '@/map/type/CommonType';
+import CesiumService from '@/map/service/CesiumService';
+import LeafletService from '@/map/service/LeafletService';
+import MapBoxService from '@/map/service/MapBoxService';
+import { namespace } from 'vuex-class';
+const appModule = namespace('appModule');
+@Component({
+    components: {
+        CesiumLineDialog
+    }
+})
 export default class LinePage extends Vue {
+    @appModule.State
+    public mapType!: MapTypeEnum;
+
+    @appModule.State
+    private cesiumMapInstance!: CesiumService;
+
+    @appModule.State
+    private leafletMapInstance!: LeafletService;
+
+    @appModule.State
+    private mapBoxMapInstance!: MapBoxService;
+
+    @appModule.Mutation
+    private setMapType!: (number: MapTypeEnum) => void;
+
+    /**
+     * 地图事件
+     * @param data
+     */
+    public async cesiumMapEvent(data: any) {
+        console.log(data);
+        switch (data.action) {
+        case 'renderLine':
+            console.log('renderLine');
+            break;
+        case 'mouseRenderLine':
+            console.log('mouseRenderLine');
+            break;
+        case 'deleteLine':
+            console.log('deleteLine');
+            break;
+        case 'close':
+            console.log('11');
+            break;
+        }
+
+    }
 }
 </script>
 
