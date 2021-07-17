74a9a47b5f3e1857209e87214a28dd94137c10c9
Marvin <yongfeide123@gmail.com>
2021-07-17 12:37:27 +0800

    feat:mapbox，leaflet面结构搭建

---
+++ src/components/dialog/cover/LeafletCoverDialog.vue
@@ -0,0 +1,94 @@
+<template lang="pug">
+    div()
+        CommonIconDialog(@change="changeStatus" v-show="!showFormContent")
+        div(class="drag marker-container" style="background: #ffffff" v-draw v-show="showFormContent")
+            i.el-icon-close.close(@click="changeStatus")
+            .buttons-container
+                //p.title 鼠标绘制
+                //el-radio-group(v-model="allowMouseRender")
+                //    el-radio(:label="true") 是
+                //    el-radio(:label="false") 否
+                p.title(v-show="allowMouseRender === false") 绘制类型
+                el-radio-group(v-model="renderType" v-show="allowMouseRender === false")
+                    el-radio(label="entity") 普通
+                    // el-radio(label="geo") geo
+            .buttons
+                el-button(type="primary" size="mini" @click="tapEvent('renderLine')") 渲染
+                el-button(type="primary" size="mini" @click="tapEvent('mouseRenderLine')" v-if="allowMouseRender") 鼠标绘制
+                el-button(type="primary" size="mini" @click="tapEvent('deleteLine')") 删除
+
+
+</template>
+
+<script lang="ts">
+import { Vue, Component, Emit } from 'vue-property-decorator';
+import CommonIconDialog from '../../../components/controlWidget/CommonIconDialog.vue';
+
+@Component({
+    name: 'LeafletCoverDialog',
+    components: {
+        CommonIconDialog
+    }
+})
+export default class LeafletCoverDialog extends Vue {
+    public visible = true;
+    public allowMouseRender = false;  // 允许鼠标绘制
+    public markerType = 1;
+    public styleType = 1;
+    public renderType = 'entity';
+    public showFormContent: boolean = true; // 是否显示
+
+    /**
+     * 事件
+     * @param type
+     */
+    @Emit('map:event')
+    public tapEvent(type: string) {
+        return {
+            action: type,
+            data: {
+                renderType: this.renderType
+            }
+        };
+    }
+
+    /**
+     * 显示状态修改
+     * @param status
+     */
+    public changeStatus(status: string) {
+        this.showFormContent = !this.showFormContent;
+    }
+}
+</script>
+
+<style scoped lang="stylus">
+.close
+    position absolute
+    top 16px
+    right 16px
+    cursor pointer
+    font-size 18px
+.marker-container
+    background #ffffff
+    border: 1px solid #909399;
+    top: 16px
+    left: 200px
+    z-index 9000 !important
+    width: 300px;
+    height: 300px;
+    border-radius: 16px;
+    padding: 0 16px 16px 16px;
+
+    .title
+        font-size: 14px;
+        font-weight: 700;
+        color: #3c4a54;
+        padding-bottom: 16px;
+        padding-top: 16px;
+
+    .buttons
+        margin-top 16px
+        display flex
+        justify-content: flex-start;
+</style>
